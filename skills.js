4587bcad132186be6491c328049b13e03c25e750
polodev <polodev10@gmail.com>
2018-11-05 23:15:32 +0600

    changed in cv

---
+++ skills.js
@@ -0,0 +1,86 @@
+var skills = [
+  {
+    technology: 'Html',
+    projects: '100+',
+    exeperiences: '5+',
+    rating: 8, 
+  },
+  {
+    technology: 'Css',
+    projects: '100+',
+    exeperiences: '5+',
+    rating: 8, 
+  },
+  {
+    technology: 'JS',
+    projects: '100+',
+    exeperiences: '5+',
+    rating: 8, 
+  },
+  {
+    technology: 'jQuery',
+    projects: '100+',
+    exeperiences: '5+',
+    rating: 8, 
+  },
+  {
+    technology: 'PHP',
+    projects: '50+',
+    exeperiences: '4+',
+    rating: 8, 
+  },
+  {
+    technology: 'MySql',
+    projects: '50+',
+    exeperiences: '4+',
+    rating: 7, 
+  },
+  {
+    technology: 'Laravel',
+    projects: '30+',
+    exeperiences: '3+',
+    rating: 8, 
+  },
+  {
+    technology: 'React js',
+    projects: '30+',
+    exeperiences: '3+',
+    rating: 8, 
+  },
+  {
+    technology: 'Vue Js',
+    projects: '20+',
+    exeperiences: '2+',
+    rating: 8, 
+  },
+  {
+    technology: 'React native',
+    projects: '5+',
+    exeperiences: '1+',
+    rating: 6, 
+  },
+  {
+    technology: 'WordPress',
+    projects: '10+',
+    exeperiences: '3+',
+    rating: 8, 
+  },
+  {
+    technology: 'Angular',
+    projects: '2+',
+    exeperiences: '1+',
+    rating: 6, 
+  },
+  {
+    technology: 'Jekyll',
+    projects: '10+',
+    exeperiences: '3+',
+    rating: 8, 
+  },
+  {
+    technology: 'Go Hugo',
+    projects: '5+',
+    exeperiences: '1+',
+    rating: 6, 
+  },
+]
